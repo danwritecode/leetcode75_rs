fd4e5d8e6f858ee44b9278974b385493f5e46a1f
danwritecode <dnelson4993@outlook.com>
2023-08-28 15:32:13 -0400

    naive implementation of greatest common divisor of string

--- greatest_common_divisor_of_strings/src/main.rs
+++ greatest_common_divisor_of_strings/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let foo = Solution::gcd_of_strings("LEET".to_string(), "CODE".to_string());
+    let foo = Solution::gcd_of_strings("ABCABCABC".to_string(), "ABCAAA".to_string());
     println!("{}", foo);
 }
 
@@ -10,6 +10,8 @@ impl Solution {
         let max_len = std::cmp::max(str1.len(), str2.len());
         let str1_len = str1.len();
         let str2_len = str2.len();
+        let str1_chars = str1.chars().collect::<Vec<char>>();
+        let str2_chars = str2.chars().collect::<Vec<char>>();
 
         let mut gcd = 0;
         for i in (1..max_len + 1).rev() {
@@ -26,7 +28,22 @@ impl Solution {
         let str2_gcd = str2[..gcd].to_string();
 
         if str1_gcd == str2_gcd {
-            return str1_gcd; // doesn't matter which you return 
+            let str1_chunked:Vec<String> = str1_chars.chunks(gcd).map(|c| c.to_vec().iter().collect::<String>()).collect();
+            let str2_chunked:Vec<String> = str2_chars.chunks(gcd).map(|c| c.to_vec().iter().collect::<String>()).collect();
+
+            for c in str1_chunked {
+                if c != str1_gcd {
+                    return "".to_string();
+                }
+            }
+
+            for c in str2_chunked {
+                if c != str1_gcd {
+                    return "".to_string();
+                }
+            }
+
+            return str1_gcd;
         }
 
         return "".to_string();
@@ -43,5 +60,6 @@ mod tests {
         assert_eq!(Solution::gcd_of_strings("ABABAB".to_string(), "ABAB".to_string()), "AB".to_string());
         assert_eq!(Solution::gcd_of_strings("LEET".to_string(), "CODE".to_string()), "".to_string());
         assert_eq!(Solution::gcd_of_strings("ABCDEF".to_string(), "ABC".to_string()), "".to_string());
+        assert_eq!(Solution::gcd_of_strings("ABCABCABC".to_string(), "ABCAAA".to_string()), "".to_string());
     }
 }
